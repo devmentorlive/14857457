09ba6634d5eb08ce56652989f6b8fd05e544eb61
Mark Tellez <mark.tellez@icloud.com>
2020-07-21 05:46:22 -0500

    can select tilesets

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -41,6 +41,7 @@ export default function App() {
       <TilePalette
         position={position}
         tileset={tileset}
+        setTileset={setTileset}
         activeTile={activeTile}
         setActiveTile={setActiveTile}
         size={{
